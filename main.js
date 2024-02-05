63c591a80a68faba981a6cb5ee2d26604c3625a0
aniyasinghaniya <105536074+aniyasinghaniya@users.noreply.github.com>
2024-02-05 17:52:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-function preload() {
+ function preload() {
+	mario_gameover = loadSound("gameover.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
 	world_start = loadSound("world_start.wav");
-	mario_gameover=loadSound("gameover.wave");
-	mario_coin=loadSound("coin.wav");
-	mario_jump=loadSound("jump.wav");
-	mario_kick=loadSound("jump.wav");
-	mario_die=loadSound("mariodie.wav");
-
 	setSprites();
 	MarioAnimation();
 }
@@ -13,28 +12,34 @@ function preload() {
 function setup() {
 	canvas = createCanvas(1240,336);
 	canvas.parent('canvas');
+
 	instializeInSetup(mario);
-	video=createCapture(VIDEO);
-	video.size(800,400);
-	video.parent('game_console')
-	poseNet = ml5.poseNet(video,modal_loaded);
-	poseNet.on('pose',gotPoses);
-
-}
-function modalloaded(){
-	console.log("modal loaded succesfully !");
-}
-function gotPoses(results){
-	if (results.length>0){
-		console.log(results);
-		nosex=results[0].pose.nose.x
-		nosey=results[0].pose.nose.y
-	}
 	
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+
 }
 
+function modelLoaded() {
+	console.log('Model Loaded!');
+  }
+
+  function gotPoses(results)
+  {
+	if(results.length > 0)
+	{
+	  console.log(results);
+	  noseX = results[0].pose.nose.x;
+	  noseY = results[0].pose.nose.y;
+	}
+  }
+   
 function draw() {
-	game()
+	game();
 }
 
 
